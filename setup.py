ce15d1257c7ee4a745bcba92488b075033130751
Samyar Noruzi <128214024+samyar89@users.noreply.github.com>
2026-02-05 23:45:34 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup
 setup(
     name='PyPhysicist',
     version='1.0.2',
-    author='Samyar Noruzi',
+    author='Samyar Norouzi',
     author_email='s.noruzi1389@gmail.com',
     description='A Physics Module for Python!',
 )
+
